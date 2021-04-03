2b25cabd4a03557b2719956f1edb77d0b07f9d33
Vitaliy <tpejib@gmail.com>
2021-04-04 01:42:19 +0300

    calculator styles adopt

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log('123');
+// import 'tom-select';
+var selectOptions = {};
+new TomSelect("#calculator__colors", selectOptions);
+new TomSelect("#calculator__size", selectOptions);
+new TomSelect("#calculator__city", selectOptions);
+new TomSelect("#calculator__delivery", selectOptions);
+var datepicker = datepicker('#calculator__datepicker-input', {
+  dateSelected: new Date()
+});
+var calcNumber = document.querySelector('#calculator__count-input');
+var calcNumberMinus = document.querySelector('#calculator__count-minus');
+var calcNumberPlus = document.querySelector('#calculator__count-plus');
+console.log(calcNumber);
+calcNumberMinus.addEventListener('click', function () {
+  console.log('minus click');
+  calcNumber.stepDown(1);
+});
+calcNumberPlus.addEventListener('click', function () {
+  console.log('plus click');
+  calcNumber.stepUp(1);
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-console.log('123');
+// import 'tom-select';
+
+const selectOptions = {}
+
+new TomSelect("#calculator__colors", selectOptions);
+new TomSelect("#calculator__size", selectOptions);
+new TomSelect("#calculator__city", selectOptions);
+new TomSelect("#calculator__delivery", selectOptions);
+
+const datepicker = datepicker('#calculator__datepicker-input', {
+    dateSelected: new Date(),
+})
+
+const calcNumber = document.querySelector('#calculator__count-input');
+const calcNumberMinus = document.querySelector('#calculator__count-minus');
+const calcNumberPlus = document.querySelector('#calculator__count-plus');
+console.log(calcNumber);
+
+calcNumberMinus.addEventListener('click', function() {
+    console.log('minus click');
+    calcNumber.stepDown(1)
+})
+calcNumberPlus.addEventListener('click', function() {
+    console.log('plus click');
+    calcNumber.stepUp(1)
+})
