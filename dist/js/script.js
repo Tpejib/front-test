294b1d8e2d0283bde87ebd8083cc1d4ab5e29c6d
Vitaliy <tpejib@gmail.com>
2021-04-04 18:34:47 +0300

    steps slider adopt

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,79 @@
 "use strict";
 
 // import 'tom-select';
+// Tom Select
 var selectOptions = {};
 new TomSelect("#calculator__colors", selectOptions);
 new TomSelect("#calculator__size", selectOptions);
 new TomSelect("#calculator__city", selectOptions);
-new TomSelect("#calculator__delivery", selectOptions);
+new TomSelect("#calculator__delivery", selectOptions); // Datepicker
+
 var datepicker = datepicker('#calculator__datepicker-input', {
-  dateSelected: new Date()
-});
+  dateSelected: new Date(),
+  position: 'tr'
+}); // Calc Number
+
 var calcNumber = document.querySelector('#calculator__count-input');
 var calcNumberMinus = document.querySelector('#calculator__count-minus');
 var calcNumberPlus = document.querySelector('#calculator__count-plus');
-console.log(calcNumber);
 calcNumberMinus.addEventListener('click', function () {
-  console.log('minus click');
   calcNumber.stepDown(1);
 });
 calcNumberPlus.addEventListener('click', function () {
-  console.log('plus click');
   calcNumber.stepUp(1);
+}); // Content Slider
+
+var stepsItems = document.querySelectorAll('.steps-content__item');
+
+var slideContent = function slideContent(slide) {
+  var list = slide.querySelectorAll('.slide-content__list-item');
+  var textList = slide.querySelectorAll('.slide-content__text');
+  list.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var listId = this.dataset.id;
+      list.forEach(function (item) {
+        return item.classList.remove('active');
+      });
+      this.classList.add('active');
+      textList.forEach(function (item) {
+        var textId = item.dataset.id;
+        item.classList.remove('active');
+
+        if (textId == listId) {
+          item.classList.add('active');
+        }
+      });
+    });
+  });
+};
+
+stepsItems.forEach(function (item) {
+  return slideContent(item);
+}); // Steps Slider
+
+var stepsRange = document.querySelector('.steps-range');
+var stepsRangeItems = stepsRange.querySelectorAll('.steps-range__item');
+var stepsContent = document.querySelectorAll('.steps-content');
+stepsRangeItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    var currentId = this.dataset.id;
+    stepsRangeItems.forEach(function (item) {
+      var itemId = item.dataset.id;
+
+      if (itemId <= currentId) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+
+      stepsContent.forEach(function (item) {
+        var contentId = item.dataset.id;
+        item.classList.remove('active');
+
+        if (contentId == currentId) {
+          item.classList.add('active');
+        }
+      });
+    });
+  });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,86 @@
 // import 'tom-select';
 
+
+
+// Tom Select
+
 const selectOptions = {}
 
-new TomSelect("#calculator__colors", selectOptions);
-new TomSelect("#calculator__size", selectOptions);
-new TomSelect("#calculator__city", selectOptions);
-new TomSelect("#calculator__delivery", selectOptions);
+new TomSelect("#calculator__colors", selectOptions)
+new TomSelect("#calculator__size", selectOptions)
+new TomSelect("#calculator__city", selectOptions)
+new TomSelect("#calculator__delivery", selectOptions)
+
+// Datepicker
 
 const datepicker = datepicker('#calculator__datepicker-input', {
     dateSelected: new Date(),
+    position: 'tr'
 })
 
-const calcNumber = document.querySelector('#calculator__count-input');
-const calcNumberMinus = document.querySelector('#calculator__count-minus');
-const calcNumberPlus = document.querySelector('#calculator__count-plus');
-console.log(calcNumber);
+// Calc Number
+
+const calcNumber = document.querySelector('#calculator__count-input')
+const calcNumberMinus = document.querySelector('#calculator__count-minus')
+const calcNumberPlus = document.querySelector('#calculator__count-plus')
 
 calcNumberMinus.addEventListener('click', function() {
-    console.log('minus click');
     calcNumber.stepDown(1)
 })
 calcNumberPlus.addEventListener('click', function() {
-    console.log('plus click');
     calcNumber.stepUp(1)
+})
+
+
+// Content Slider
+
+const stepsItems = document.querySelectorAll('.steps-content__item')
+
+const slideContent = function(slide) {
+    const list = slide.querySelectorAll('.slide-content__list-item')
+    const textList = slide.querySelectorAll('.slide-content__text')
+
+    list.forEach(item => {
+        item.addEventListener('click', function() {
+            let listId = this.dataset.id
+            list.forEach(item => item.classList.remove('active'))
+            this.classList.add('active')
+            textList.forEach(function(item) {
+                let textId = item.dataset.id
+                item.classList.remove('active')
+                if (textId == listId) {
+                    item.classList.add('active')
+                }
+            })
+        })
+    })
+}
+
+stepsItems.forEach(item => slideContent(item))
+
+// Steps Slider
+
+const stepsRange = document.querySelector('.steps-range')
+const stepsRangeItems = stepsRange.querySelectorAll('.steps-range__item')
+const stepsContent = document.querySelectorAll('.steps-content')
+
+stepsRangeItems.forEach(item => {
+    item.addEventListener('click', function() {
+        let currentId = this.dataset.id
+        stepsRangeItems.forEach(item => {
+            let itemId = item.dataset.id
+            if (itemId <= currentId) {
+                item.classList.add('active')
+            } else {
+                item.classList.remove('active')
+            }
+            stepsContent.forEach(function(item) {
+                let contentId = item.dataset.id
+                item.classList.remove('active')
+                if (contentId == currentId) {
+                    item.classList.add('active')
+                }
+            })
+        })
+    })
 })
